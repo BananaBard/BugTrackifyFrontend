c979f3d4c20ade994b3f11df138517e0233637b6
BananaBard <brunogonzalez.dev@gmail.com>
2024-06-27 22:06:55 -0300

    Updated incident reports placeholder Now it uses types from types.d.ts

--- src/api placeholder/data.ts
+++ src/api placeholder/data.ts
@@ -1,25 +1,4 @@
-type Incident = {
-  id: string;
-  title: string;
-  project: string;
-  creationDate: string;
-  assignedTo: string;
-  createdBy: string;
-  tag: string;
-  priority: string;
-  severity: string;
-  description: string;
-  stepsToReproduce: string[];
-  actualResult: string;
-  expectedResult: string;
-  comments: UserComment[];
-};
-
-type UserComment = {
-  creator: string;
-  content: string;
-};
-
+import { type Incident } from "@/types";
 export const incidentReports: Incident[] = [
   {
     id: "1",

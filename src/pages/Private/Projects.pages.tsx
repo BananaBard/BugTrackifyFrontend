5c1be598f839f51b4c1bfe6a5e74d06dd94f13b7
Bruno González <brunogonzalez.dev@gmail.com>
2024-06-29 20:44:09 -0300

    Starting to layout the projects page

--- src/pages/Private/Projects.pages.tsx
+++ src/pages/Private/Projects.pages.tsx
@@ -2,30 +2,36 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/com
 import { projects } from '../../api placeholder/data';
 import DashboardBreadcrum from '@/components/DashboardBreadcrum';
 import useHistory from '@/hooks/onUseHistory';
+import { Button } from '@/components/ui/button';
+import { PlusIcon } from 'lucide-react';
+import { Input } from '@/components/ui/input';
+import ProjectCard from '@/components/cards/ProjectCard';
 
 const ProjectsPage = () => {
     const history = useHistory();
 
     return (
         <main className='p-8 w-full h-screen bg-neutral-100 dark:bg-neutral-800'>
-            <DashboardBreadcrum history={history}/>
+            <header className='flex justify-between items-center'>
+                <DashboardBreadcrum history={history} />
+                <div className="relative ml-auto flex-1 md:grow-0">
+                    <div className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+                    <Input
+                        type="search"
+                        placeholder="Search projects..."
+                        className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
+                    />
+                </div>
+                <Button variant='default' className='ml-4'>
+                    <PlusIcon className="h-4 w-4 mr-2" />
+                    New Project
+                </Button>
+            </header>
             <h1 className='text-2xl mb-4'>Your projects</h1>
-            <ul className='flex flex-col gap-y-4'>
+            <ul className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
                 {
                     projects.map(p => (
-                        <Card key={p.projectId} className='max-w-xl relative'>
-                            <CardHeader>
-                                <CardTitle>{p.name}</CardTitle>
-                                <CardDescription>{p.description}</CardDescription>
-                            </CardHeader>
-                            <CardContent className='flex gap-4 items-center justify-between'>
-                                <p>{p.status}</p>
-                                <p className='absolute top-6 right-4'>{p.startDate}</p>
-                                <p>Members: {p.team.length}</p>
-                                <p>Leader: {p.leader}</p>
-                            </CardContent>
-                        </Card>
-
+                        <ProjectCard key={p.projectId}/>
                     ))
                 }
             </ul>

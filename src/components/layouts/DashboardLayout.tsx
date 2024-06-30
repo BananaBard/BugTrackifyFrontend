0b143ce29ec5b0b3cf4d2abf5f4b070e42d335b1
Bruno González <brunogonzalez.dev@gmail.com>
2024-06-29 21:41:35 -0300

    Added sidebar provider to dashboard layout

--- src/components/layouts/DashboardLayout.tsx
+++ src/components/layouts/DashboardLayout.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom"
 import { ModeToggle } from "../ModeToggle"
 import Sidebar from "../Sidebars/Sidebar"
+import SidebarProvider from "@/context/SidebarContext"
 
 const DashboardLayout = () => {
     return (
         <div className="flex">
-            <Sidebar/>
+            <SidebarProvider>
+                <Sidebar/>
+            </SidebarProvider>
             <span className="absolute top-2 right-2">
                 <ModeToggle />
             </span>

862aec2437331b33e10bb03a8b88e45416794ceb
Bruno González <brunogonzalez.dev@gmail.com>
2024-07-02 00:26:36 -0300

    Created path cleaner func For the moment unused, could be handy later

--- src/hooks/onUseHistory.tsx
+++ src/hooks/onUseHistory.tsx
@@ -4,7 +4,25 @@ const useHistory = (): string[] => {
     const {pathname} =  useLocation()
     const history = pathname.trim().split('/').filter(i => i != "");
 
+   /*  if (history.length > 3) {
+        history.pop()
+        history[2] = cleanPath(history[2])
+    }
+     */
     return history
 }
+
+/* const cleanPath = (path: string): string => {
+    let cleanedPath = path;
+
+    if (cleanedPath.endsWith(':')) {
+        cleanedPath = cleanedPath.slice(0, -1);
+    }
+    if (cleanedPath.startsWith(':')) {
+        cleanedPath = cleanedPath.slice(1);
+    }
+
+    return decodeURIComponent(cleanedPath)
+} */
 
 export default useHistory

ccf83105dbe1d338c056d5eac54f4a686aadd087
Bruno González <brunogonzalez.dev@gmail.com>
2024-07-08 20:12:20 -0300

    Updated signup The services now takes more arguments, added the same to the form.

--- src/components/forms/SignUpForm.tsx
+++ src/components/forms/SignUpForm.tsx
@@ -5,23 +5,33 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
 import { useAuth } from "@/context/AuthContext";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select";
 
+const userRoles = ['Developer', 'Tester', 'Project Leader'] as const;
 
 const formSchema = z.object({
   email: z.string().email('Invalidad email address'),
   password: z.string().min(6, 'Password must be 6 characters long').max(32, 'Can not be longer than 32 characters'),
-  password2: z.string()
+  password2: z.string(),
+  fullname: z.string().min(6).max(64, 'Cant be longer than 64 characters'),
+  role: z.enum(userRoles)
+
 }).refine((data) => data.password === data.password2, {
   message: "Passwords must match",
   path: ["password2"]
 })
 
 const SignUpForm = () => {
-  const {signUpWithEmail} = useAuth();
+  const { signUpWithEmail } = useAuth();
 
   const onSubmit = (values: z.infer<typeof formSchema>) => {
     console.log(values)
-    const body = {email: values.email, password: values.password}
+    const body = {
+      email: values.email,
+      password: values.password,
+      fullname: values.fullname,
+      role: values.role
+    }
     signUpWithEmail(body);
   }
   const form = useForm<z.infer<typeof formSchema>>({
@@ -29,7 +39,8 @@ const SignUpForm = () => {
     defaultValues: {
       email: '',
       password: '',
-      password2: ''
+      password2: '',
+      fullname: ''
     }
   })
 
@@ -44,7 +55,7 @@ const SignUpForm = () => {
             <FormItem>
               <FormLabel>Email</FormLabel>
               <FormControl>
-                <Input placeholder="Your email..." {...field}/>
+                <Input placeholder="Your email..." {...field} />
               </FormControl>
               {/* <FormDescription>The email you will use to log in</FormDescription> */}
               <FormMessage />
@@ -52,31 +63,62 @@ const SignUpForm = () => {
           )}
         >
         </FormField>
-        <FormField control={form.control} name='password' render={({field}) => (
+        <FormField control={form.control} name='password' render={({ field }) => (
           <FormItem>
             <FormLabel>Password</FormLabel>
             <FormControl>
-              <Input placeholder="Password" type="password" {...field}/>
+              <Input placeholder="Password" type="password" {...field} />
             </FormControl>
             <FormDescription>Between 6/32 characters</FormDescription>
-            <FormMessage/>
+            <FormMessage />
           </FormItem>
-        )}>        
+        )}>
         </FormField>
-        <FormField control={form.control} name='password2' render={({field}) => (
+        <FormField control={form.control} name='password2' render={({ field }) => (
           <FormItem>
             <FormLabel>Confirm Password</FormLabel>
             <FormControl>
-              <Input placeholder="Confirm your password" type="password" {...field}/>
+              <Input placeholder="Confirm your password" type="password" {...field} />
             </FormControl>
-            <FormMessage/>
+            <FormMessage />
           </FormItem>
         )}></FormField>
-        <FormMessage/>
+        <FormField control={form.control} name='fullname' render={({ field }) => (
+          <FormItem>
+            <FormLabel>Your name</FormLabel>
+            <FormControl>
+              <Input placeholder="Jhon Doe" type="text" {...field} />
+            </FormControl>
+            <FormDescription>Use your full name</FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}>
+        </FormField>
+        <FormField control={form.control} name='role' render={({ field }) => (
+          <FormItem>
+            <FormLabel>Role</FormLabel>
+            <FormControl>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <SelectTrigger>
+                  <SelectValue placeholder="Select your role" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="Developer">Developer</SelectItem>
+                  <SelectItem value="Tester">Tester</SelectItem>
+                  <SelectItem value="Project Leader">Project Leader</SelectItem>
+                </SelectContent>
+              </Select>
+            </FormControl>
+            <FormDescription>This can not be changed later</FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}>
+        </FormField>
+        <FormMessage />
         <Button type="submit" className="w-full mt-4">Create account</Button>
       </form>
     </Form>
   )
 }
 
-  export default SignUpForm;
+export default SignUpForm;
--- src/services/auth/signUpWithEmail.ts
+++ src/services/auth/signUpWithEmail.ts
@@ -3,15 +3,19 @@ import getErrorMessage from "@/lib/utils";
 interface SignUpWithEmailServiceArgs {
     email: string;
     password: string;
+    role: 'Developer' | 'Tester' | 'Project Leader';
+    fullname: string;
 }
 
-const signUpWithEmailService = async({email, password}: SignUpWithEmailServiceArgs) => {
+const signUpWithEmailService = async({email, password, role, fullname}: SignUpWithEmailServiceArgs) => {
     try {
         const res = await fetch('http://localhost:3000/auth/signup', {
             method: 'POST',
             body: JSON.stringify({
                 email: email,
-                password: password
+                password: password,
+                fullname: fullname,
+                role: role
             }),
             headers: {
                 "Content-Type": "application/json"

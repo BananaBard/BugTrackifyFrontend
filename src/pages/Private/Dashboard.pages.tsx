571cd02df62382433b0ebceb4f3257c63b92d436
Bruno González <brunogonzalez.dev@gmail.com>
2024-07-08 18:53:42 -0300

    Added user from auth context

--- src/pages/Private/Dashboard.pages.tsx
+++ src/pages/Private/Dashboard.pages.tsx
@@ -1,9 +1,11 @@
+import { useAuth } from "@/context/AuthContext"
 
 const DashboardPage = () => {
-
+    const {user} = useAuth();
+    console.log(user)
     return (
         <>
-            <h1 className="text-2xl mb-4">Welcome, Bruno!</h1>
+            <h1 className="text-2xl mb-4">Welcome, {user?.fullname}!</h1>
         </>
     )
 }

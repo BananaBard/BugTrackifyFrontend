61d2ba36813176a345b7f3e30b9d532f6668a020
Bruno González <brunogonzalez.dev@gmail.com>
2024-07-09 20:53:47 -0300

    Check if we dont have user to show sign up and log in pages

--- src/routes/Public.routes.tsx
+++ src/routes/Public.routes.tsx
@@ -1,16 +1,24 @@
+import { useAuth } from "@/context/AuthContext"
 import LoginPage from "@/pages/Auth/Login.pages"
 import SignUpPage from "@/pages/Auth/SignUp.pages"
 import LandingPage from "@/pages/Landing.pages"
 import { Route, Routes } from "react-router-dom"
 
 const PublicRoutes = () => {
+    const { user } = useAuth();
     return (
         <Routes>
-            <Route path="/" element={<LandingPage/>}/>
-            <Route path="/login" element={<LoginPage/>}/>
-            <Route path="/signup" element={<SignUpPage/>}/>
+            <Route path="/" element={<LandingPage />} />
+            {
+                !user && (
+                    <>
+                        <Route path="/login" element={<LoginPage />} />
+                        <Route path="/signup" element={<SignUpPage />} />
+                    </>
+                )
+            }
         </Routes>
     )
-}  
+}
 
 export default PublicRoutes

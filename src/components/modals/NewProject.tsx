75e4483e944e50735b1432822fab4c6cc2721a9d
Bruno González <brunogonzalez.dev@gmail.com>
2024-07-14 19:35:00 -0300

    feat: Add new project dialog

---
+++ src/components/modals/NewProject.tsx
@@ -0,0 +1,31 @@
+import {
+    Dialog,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import { Button } from "../ui/button"
+import { PlusIcon } from "lucide-react"
+import CreateProjectForm from "../forms/CreateProjectForm"
+
+const NewProjectDialog = () => {
+    return (
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button variant='default' className='sm:ml-4 min-[480px]:w-1/4 md:w-auto'>
+                    <PlusIcon className="h-4 w-4 mr-2" />
+                    New Project
+                </Button>
+            </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Create a new project!</DialogTitle>
+                    <CreateProjectForm/>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
+    )
+}
+
+export default NewProjectDialog

62921f47517866dbda9c039b09f40e1f21c21d41
Bruno González <brunogonzalez.dev@gmail.com>
2024-07-14 19:34:30 -0300

    feat: Change in styles and rendering Now we render the members by role, fixed the styles to display the information in a responsive manner

--- src/pages/Private/Project.pages.tsx
+++ src/pages/Private/Project.pages.tsx
@@ -5,43 +5,60 @@ import { Input } from "@/components/ui/input"
 import { PlusIcon } from "lucide-react"
 
 
-
 const ProjectPage = () => {
     const data = projects[0];
+    const testers = data.team.filter(member => member.role === 'Tester')
+    const developers = data.team.filter(member => member.role === 'Developer')
     return (
         <>
-            <header className='flex justify-between items-center mb-4'>
-                <h1 className='text-2xl '>{data.title}</h1>
-                <div className="relative ml-auto flex-1 md:grow-0">
-                    <div className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-                    <Input
-                        type="search"
-                        placeholder="Search incidents..."
-                        className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
-                    />
+            <header className='flex flex-col sm:flex-row items-start gap-y-2 sm:justify-between sm:items-center mb-4'>
+                <h1 className='lg:text-2xl'>{data.title}</h1>
+                <div className='flex flex-col gap-y-2 w-full sm:w-fit min-[480px]:flex-row min-[480px]:gap-x-2'>
+                    <div className="relative w-full  sm:w-auto sm:ml-auto flex-1 md:grow-0">
+                        <div className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+                        <Input
+                            type="search"
+                            placeholder="Search incidents..."
+                            className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
+                        />
+                    </div>
+                    <Button variant='default' className='sm:ml-4 min-[480px]:w-1/4 md:w-auto'>
+                        <PlusIcon className="h-4 w-4 mr-2" />
+                        New Incident
+                    </Button>
                 </div>
-                <Button variant='default' className='ml-4'>
-                    <PlusIcon className="h-4 w-4 mr-2" />
-                    New Incident
-                </Button>
             </header>
-            <div className="flex gap-4">
-                <div className="w-1/4 flex flex-col gap-2">
-                    <div className="w-full border rounded-md py-6 px-4 dark:bg-stone-900">
+            <div className="flex flex-col md:flex-row gap-4">
+                <div className="w-full flex flex-col min-[480px]:flex-row md:w-1/4 lg:w-1/6 md:flex-col gap-2">
+                    <div className="w-full border rounded-md p-4 dark:bg-stone-900">
                         <h3 className="text-xl font-medium">Leader</h3>
                         <p>{data.leader}</p>
                     </div>
-                    <div className="w-full border rounded-md py-6 px-4 dark:bg-stone-900">
-                        <h4 className="text-xl mb-2 font-medium">Members</h4>
+                    <div className="w-full border rounded-md py-6 px-4 dark:bg-stone-900 max-h-64 min-[480px]:max-h-48 md:max-h-64 overflow-y-scroll">
+                        <h3 className="text-xl font-medium">Members</h3>
+                        <div className="flex flex-col gap-y-2 sm:flex-row sm:gap-x-16 sm:items-start md:flex-col md:items-start">
+                            <div className="flex flex-col">
+                                <h4 className="underline text-lg">Testers</h4>
+                                <ul>
+                                    {testers.map((member, index) => {
+                                        return <li key={index} className="sm:text-md">{member.fullname}</li>
+                                    })}
+                                </ul>
+                            </div>
 
-                        <ul>
-                            {data.team.map(member => {
-                                return <li key={member}>{member}</li>
-                            })}
-                        </ul>
+                            <div className="lg:mt-2 flex flex-col">
+                                <h4 className="underline text-lg">Developers</h4>
+                                <ul>
+                                    {developers.map((member, index) => {
+                                        return <li key={index} className="sm:text-md">{member.fullname}</li>
+                                    })}
+                                </ul>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
-                <div className="w-3/4">
+                <div className="w-full md:w-3/4 lg:w-5/6 ">
                     <PreviewIncidentListing incidents={bugs} />
                 </div>
             </div>

3e7d3fd8fca851acf6723a021dcd73acd165e66f
Bruno González <brunogonzalez.dev@gmail.com>
2024-07-14 19:31:55 -0300

    feat: add sonner toast

--- src/Main.tsx
+++ src/Main.tsx
@@ -4,12 +4,15 @@ import "./style.css";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "./context/ThemeContext";
 import AuthProvider from "./context/AuthContext";
+import {Toaster} from 'sonner';
+
 
 const root = createRoot(document.getElementById("app")!);
 root.render(
   <BrowserRouter>
     <ThemeProvider>
       <AuthProvider>
+        <Toaster position="top-center" closeButton={true} richColors/>
         <App />
       </AuthProvider>
     </ThemeProvider>

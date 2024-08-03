fbb19fe76083ba350923697a658bee3d65214125
Bruno González <brunogonzalez.dev@gmail.com>
2024-08-03 18:07:30 -0300

    feat: add react query Set up react query in Main.tsx

--- src/Main.tsx
+++ src/Main.tsx
@@ -4,17 +4,23 @@ import "./style.css";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "./context/ThemeContext";
 import AuthProvider from "./context/AuthContext";
-import {Toaster} from 'sonner';
+import { Toaster } from 'sonner';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 
+const queryClient = new QueryClient();
 
 const root = createRoot(document.getElementById("app")!);
 root.render(
   <BrowserRouter>
     <ThemeProvider>
-      <AuthProvider>
-        <Toaster position="top-center" closeButton={true} richColors/>
-        <App />
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <Toaster position="top-center" closeButton={true} richColors />
+          <App />
+          <ReactQueryDevtools initialIsOpen={false}/>
+        </AuthProvider>
+      </QueryClientProvider>
     </ThemeProvider>
   </BrowserRouter>
 );

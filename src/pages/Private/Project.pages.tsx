714a3cb693502a80bafb5a5cb5aab0817d73a66c
Bruno González <brunogonzalez.dev@gmail.com>
2024-08-06 18:56:22 -0300

    feat: Add useProject hook to projects pages

--- src/pages/Private/Project.pages.tsx
+++ src/pages/Private/Project.pages.tsx
@@ -1,16 +1,39 @@
-import { projects, incidentsReports as bugs } from "@/api placeholder/data"
+import { incidentsReports as bugs } from "@/api placeholder/data"
 import PreviewIncidentListing from "@/components/listings/PreviewIncidentListing"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
+import useProjects from "@/hooks/useProjects"
 import { PlusIcon } from "lucide-react"
+import { useParams } from "react-router-dom"
 
+const team = [
+    {
+      fullname: "Alice Johnson", role: 'Project Leader'
+    },
+    {
+      fullname: "Bob Smith", role: 'Developer'
+    },
+    {
+      fullname: "Charlie Brown", role: 'Tester'
+    },
+    {
+      fullname: "Juan Arias", role: 'Developer'
+    },
+    {
+      fullname: "Roberto Sanchez", role: 'Tester'
+    },
+  ]
 
 const ProjectPage = () => {
-    const data = projects[0];
-    const testers = data.team.filter(member => member.role === 'Tester')
-    const developers = data.team.filter(member => member.role === 'Developer')
+    const {projectId} = useParams();
+    const {data: projects} = useProjects();
+    const data = projects?.find(p => p.id == projectId)
+
+    const testers =  team.filter(member => member.role === 'Tester') /* data?.team ? data.team.filter(member => member.role === 'Tester') : null; */
+    const developers = team.filter(member => member.role === 'Developer') /* data?.team ? data.team.filter(member => member.role === 'Developer') : null; */
     return (
-        <>
+            data && (
+                <>
             <header className='flex flex-col sm:flex-row items-start gap-y-2 sm:justify-between sm:items-center mb-4'>
                 <h1 className='lg:text-2xl'>{data.title}</h1>
                 <div className='flex flex-col gap-y-2 w-full sm:w-fit min-[480px]:flex-row min-[480px]:gap-x-2'>
@@ -32,7 +55,7 @@ const ProjectPage = () => {
                 <div className="w-full flex flex-col min-[480px]:flex-row md:w-1/4 lg:w-1/6 md:flex-col gap-2">
                     <div className="w-full border rounded-md p-4 dark:bg-stone-900">
                         <h3 className="text-xl font-medium">Leader</h3>
-                        <p>{data.leader}</p>
+                        <p>{data.leader.fullname}</p>
                     </div>
                     <div className="w-full border rounded-md py-6 px-4 dark:bg-stone-900 max-h-64 min-[480px]:max-h-48 md:max-h-64 overflow-y-scroll">
                         <h3 className="text-xl font-medium">Members</h3>
@@ -40,7 +63,7 @@ const ProjectPage = () => {
                             <div className="flex flex-col">
                                 <h4 className="underline text-lg">Testers</h4>
                                 <ul>
-                                    {testers.map((member, index) => {
+                                    {testers?.map((member, index) => {
                                         return <li key={index} className="sm:text-md">{member.fullname}</li>
                                     })}
                                 </ul>
@@ -49,20 +72,26 @@ const ProjectPage = () => {
                             <div className="lg:mt-2 flex flex-col">
                                 <h4 className="underline text-lg">Developers</h4>
                                 <ul>
-                                    {developers.map((member, index) => {
+                                    {developers?.map((member, index) => {
                                         return <li key={index} className="sm:text-md">{member.fullname}</li>
                                     })}
                                 </ul>
                             </div>
-
                         </div>
                     </div>
                 </div>
                 <div className="w-full md:w-3/4 lg:w-5/6 ">
-                    <PreviewIncidentListing incidents={bugs} />
+                    {
+                        bugs ? (
+                            <PreviewIncidentListing incidents={bugs} />
+                        ) : (
+                            <h2>This project does not have any incidents yet.</h2>
+                        )
+                    }
                 </div>
             </div>
         </>
+            )
     )
 }
 
